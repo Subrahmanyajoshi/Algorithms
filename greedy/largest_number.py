9979216191e1ce210db022810fe90ffb8a000b5b
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2022-03-10 20:59:27 +0530

    Largest Number problem

--- greedy/largest_number.py
+++ greedy/largest_number.py
@@ -1,17 +1,39 @@
-#Uses python3
-
 import sys
+from typing import List
 
-def largest_number(a):
-    #write your code here
-    res = ""
-    for x in a:
-        res += x
-    return res
+
+class LargestNumber(object):
+
+    @staticmethod
+    def compare(num1, num2):
+        max_num = num1
+        num1_len = len(num1)
+        num2_len = len(num2)
+
+        min_len = num1_len if num1_len > num2_len else num2_len
+        for i in range(min_len):
+            if num2[i] > num1[i]:
+                max_num = num2
+        return int(max_num)
+
+    @staticmethod
+    def largest_number(a: List):
+        res = ""
+        while len(a) != 0:
+            max_value = a[0]
+            for value in a:
+                max_value = LargestNumber.compare(str(value), str(max_value))
+            res += str(max_value)
+            max_index = a.index(max_value)
+            _ = a.pop(max_index)
+
+        return res
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = input.split()
-    a = data[1:]
-    print(largest_number(a))
-    
+    # input = sys.stdin.read()
+    # data = input.split()
+    # a = data[1:]
+
+    a = [21, 2]
+    print(LargestNumber.largest_number(a))
