a32603380d9795c809699531d5116bce1f7529e0
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2022-03-14 22:50:55 +0530

    Multiple Updates
    1. Updated binary search code
    2.  Added solution to binary search first occurrence problem
    3. Updated README.md

--- searching_algorithms/binary_search/binary_search_first_occur.py
+++ searching_algorithms/binary_search/binary_search_first_occur.py
@@ -7,6 +7,7 @@ class BinarySearch(object):
     def __init__(self, array: List, key: int):
         self.array = array
         self.key = key
+        self.first_index = len(array)
 
     def search(self, low, high):
         if high < low:
@@ -14,9 +15,10 @@ class BinarySearch(object):
         mid = math.floor(low + ((high - low) / 2))
 
         if self.array[mid] == self.key:
-            return mid
+            if mid < self.first_index:
+                self.first_index = mid
 
-        if self.key < self.array[mid]:
+        if self.key <= self.array[mid]:
             return self.search(low=low, high=mid - 1)
         else:
             return self.search(low=mid + 1, high=high)
@@ -24,13 +26,15 @@ class BinarySearch(object):
 
 def main():
     # Binary search requires that the input array is sorted.
-    array = [1, 5, 8, 12, 13]
-    keys = [8, 1, 23, 1, 11]
+
+    array = [2, 4, 4, 4, 7, 7, 9]
+    keys = [9, 4, 5, 2]
     results = []
 
     for key in keys:
         binary_search = BinarySearch(array=array, key=key)
-        results.append(binary_search.search(0, len(array) - 1))
+        _ = binary_search.search(0, len(array) - 1)
+        results.append(binary_search.first_index)
     print(results)
 
 
