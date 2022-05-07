40cade78144d9f1e2a134815bea5752b315510ba
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2022-05-07 13:30:03 +0530

    Add: Reversing a linked list

--- data_structures/linked_lists/basic_ll.py
+++ data_structures/linked_lists/basic_ll.py
@@ -21,7 +21,7 @@ class LinkedList(object):
             if print_ptr.next:
                 print_str += f"{print_ptr.val} -> "
             else:
-                print_str += f"{print_ptr.val}"
+                print_str += f"{print_ptr.val}\n"
             print_ptr = print_ptr.next
         return print_str
 
@@ -42,12 +42,34 @@ class LinkedList(object):
         location_ptr.next = location_ptr.next.next
         del del_ptr
 
+    def reverse(self):
+        previous = None
+        current = self.head_ptr.next
+        following = current.next
+
+        while current:
+            current.next = previous
+            previous = current
+            current = following
+            if following:
+                following = following.next
+
+        self.head_ptr = Node()
+        self.head_ptr.next = previous
+
 
 def main():
     linked_list = LinkedList(head=Node())
-    linked_list.insert(values=[1, 2, 4, 2, 2, 5])
+
+    print("Linked list")
+    linked_list.insert(values=[1, 2, 4, 7, 8, 5])
     print(linked_list)
 
+    print("Reverse: ")
+    linked_list.reverse()
+    print(linked_list)
+
+    print("Delete 3rd element")
     linked_list.delete(3)
     print(linked_list)
 
